314bf5c6bd8f937f7eaef3b947d7e3e55603aeca
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 13:56:09 +0300

    WIP: add hard coded routes for reporter

--- front-end/sacchon-app/src/app/app-routing.module.ts
+++ front-end/sacchon-app/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContentComponent } from './content/content.component';
 import { DatatableComponent } from './datatable/datatable.component';
 import { LoginComponent } from './login/login.component';
+import { AddDoctorComponent } from './reporter/add-doctor/add-doctor.component';
+import { InspectDoctorComponent } from './reporter/inspect-doctor/inspect-doctor.component';
+import { InspectPatientComponent } from './reporter/inspect-patient/inspect-patient.component';
 import { ReporterComponent } from './reporter/reporter.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -12,6 +15,9 @@ const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
 	{ path: 'signup', component: SignupComponent },
 	{ path: 'reporter', component: ReporterComponent },
+	{ path: 'reporter/doctors', component: InspectDoctorComponent },
+	{ path: 'reporter/patients', component: InspectPatientComponent },
+	{ path: 'reporter/adddoctor', component: AddDoctorComponent },
 ];
 
 @NgModule({
