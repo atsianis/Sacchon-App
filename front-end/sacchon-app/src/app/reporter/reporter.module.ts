c9221e21a43ff52c4a8f35a634b43383ca49021b
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 14:02:17 +0300

    Import datatables to reporter

--- front-end/sacchon-app/src/app/reporter/reporter.module.ts
+++ front-end/sacchon-app/src/app/reporter/reporter.module.ts
@@ -5,11 +5,15 @@ import { AddDoctorComponent } from './add-doctor/add-doctor.component';
 import { InspectDoctorComponent } from './inspect-doctor/inspect-doctor.component';
 import { InspectPatientComponent } from './inspect-patient/inspect-patient.component';
 
+import { DataTablesModule } from 'angular-datatables';
+
+
 
 @NgModule({
 	declarations: [AddDoctorComponent, InspectDoctorComponent, InspectPatientComponent],
 	imports: [
 		CommonModule,
+		DataTablesModule
 	]
 })
 export class ReporterModule { }
