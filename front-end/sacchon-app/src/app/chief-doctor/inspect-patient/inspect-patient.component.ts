1dc43e1f4dab6f4a80f063f34d3e0ddd1ad126a0
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 12:19:45 +0300

    Add inspect patient component

---
+++ front-end/sacchon-app/src/app/chief-doctor/inspect-patient/inspect-patient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-inspect-patient',
+  templateUrl: './inspect-patient.component.html',
+  styleUrls: ['./inspect-patient.component.scss']
+})
+export class InspectPatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
