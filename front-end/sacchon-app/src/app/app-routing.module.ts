e5d1b72e969b52171cbe4a892b17b9bfb6cb8a31
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 12:15:58 +0300

    Add routing skeleton for chief doctor

--- front-end/sacchon-app/src/app/app-routing.module.ts
+++ front-end/sacchon-app/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddDoctorComponent } from './chief-doctor/add-doctor/add-doctor.component';
 import { ChiefDoctorComponent } from './chief-doctor/chief-doctor.component';
+import { InspectDoctorComponent } from './chief-doctor/inspect-doctor/inspect-doctor.component';
+import { InspectPatientComponent } from './chief-doctor/inspect-patient/inspect-patient.component';
 import { ContentComponent } from './content/content.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -9,7 +12,11 @@ const routes: Routes = [
 	{ path: '', component: ContentComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'signup', component: SignupComponent },
-	{ path: 'chiefDoctor', component: ChiefDoctorComponent },
+	{ path: 'reporter', component: ChiefDoctorComponent, children: [
+		{ path: 'doctors', component: InspectDoctorComponent },
+		{ path: 'newdoctor', component: AddDoctorComponent },
+		{ path: 'patients', component: InspectPatientComponent },
+	]},
 ];
 
 @NgModule({
