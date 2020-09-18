2a3c9cf3cdeba1e17e654feac801343ac77e5ecf
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 13:55:49 +0300

    Remove reporter router

--- front-end/sacchon-app/src/app/reporter/reporter.module.ts
+++ front-end/sacchon-app/src/app/reporter/reporter.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ReporterRoutingModule } from './reporter-routing.module';
 import { AddDoctorComponent } from './add-doctor/add-doctor.component';
 import { InspectDoctorComponent } from './inspect-doctor/inspect-doctor.component';
 import { InspectPatientComponent } from './inspect-patient/inspect-patient.component';
-import { ReporterComponent } from './reporter.component';
 
 
 @NgModule({
-	declarations: [AddDoctorComponent, InspectDoctorComponent, InspectPatientComponent, ReporterComponent],
+	declarations: [AddDoctorComponent, InspectDoctorComponent, InspectPatientComponent],
 	imports: [
 		CommonModule,
-		ReporterRoutingModule
 	]
 })
 export class ReporterModule { }
