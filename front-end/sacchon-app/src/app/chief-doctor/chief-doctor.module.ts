7d676c9e140a1ba4320e14cbf10373d7b40963c6
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-18 12:18:49 +0300

    Add new components to module

--- front-end/sacchon-app/src/app/chief-doctor/chief-doctor.module.ts
+++ front-end/sacchon-app/src/app/chief-doctor/chief-doctor.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChiefDoctorComponent } from './chief-doctor.component';
+import { InspectDoctorComponent } from './inspect-doctor/inspect-doctor.component';
+import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { InspectPatientComponent } from './inspect-patient/inspect-patient.component';
 
 
 
 @NgModule({
-	declarations: [ChiefDoctorComponent],
+	declarations: [
+		ChiefDoctorComponent,
+		InspectDoctorComponent,
+		AddDoctorComponent,
+		InspectPatientComponent
+	],
 	imports: [
 		CommonModule
 	],
 	exports: [
-		ChiefDoctorComponent
+		ChiefDoctorComponent,
+		InspectDoctorComponent,
+		AddDoctorComponent,
+		InspectPatientComponent
 	]
 })
 export class ChiefDoctorModule { }
