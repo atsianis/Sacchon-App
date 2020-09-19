59a017dbb7ceefa822cba5845e5d388decedc277
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-19 18:55:07 +0300

    Add edit patient record component

---
+++ front-end/sacchon-app/src/app/medi-data-repo/edit-patient-record/edit-patient-record.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'sacchon-app-edit-patient-record',
+	templateUrl: './edit-patient-record.component.html',
+	styleUrls: ['./edit-patient-record.component.scss']
+})
+export class EditPatientRecordComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
