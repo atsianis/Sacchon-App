0d34aa2f7e79265e7f39ee4857f0d3b21152b8ca
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-19 22:25:04 +0300

    Import new components for reporter

--- front-end/sacchon-app/src/app/reporter/reporter.module.ts
+++ front-end/sacchon-app/src/app/reporter/reporter.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { CreateDoctorComponent } from './add-doctor/add-doctor.component';
 import { InspectDoctorComponent } from './inspect-doctor/inspect-doctor.component';
 import { InspectPatientComponent } from './inspect-patient/inspect-patient.component';
 
 import { DataTablesModule } from 'angular-datatables';
-
-
+import { InspectDoctorListComponent } from './inspect-doctor/inspect-doctor-list/inspect-doctor-list.component';
+import { InspectPatientListComponent } from './inspect-patient/inspect-patient-list/inspect-patient-list.component';
 
 @NgModule({
-	declarations: [AddDoctorComponent, InspectDoctorComponent, InspectPatientComponent],
+	declarations: [
+		CreateDoctorComponent,
+		InspectDoctorComponent,
+		InspectPatientComponent,
+		InspectDoctorListComponent,
+		InspectPatientListComponent
+	],
 	imports: [
 		CommonModule,
 		DataTablesModule
