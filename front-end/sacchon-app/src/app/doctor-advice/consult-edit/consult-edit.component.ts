9d962cea4d08731db0d0c59857f35080cdaad384
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-19 22:27:33 +0300

    Add consult edit component

---
+++ front-end/sacchon-app/src/app/doctor-advice/consult-edit/consult-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-consult-edit',
+  templateUrl: './consult-edit.component.html',
+  styleUrls: ['./consult-edit.component.scss']
+})
+export class ConsultEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
