ba27a9c8d40207e78a66c811f77c3cf83e104bcd
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-20 21:05:24 +0300

    Add mock view for reporter inspect doctor list

--- front-end/sacchon-app/src/app/reporter/inspect-doctor/inspect-doctor-list/inspect-doctor-list.component.ts
+++ front-end/sacchon-app/src/app/reporter/inspect-doctor/inspect-doctor-list/inspect-doctor-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'sacchon-app-inspect-doctor-list',
-  templateUrl: './inspect-doctor-list.component.html',
-  styleUrls: ['./inspect-doctor-list.component.scss']
+	selector: 'sacchon-app-inspect-doctor-list',
+	templateUrl: './inspect-doctor-list.component.html',
+	styleUrls: ['./inspect-doctor-list.component.scss']
 })
 export class InspectDoctorListComponent implements OnInit {
+	dtOptions: DataTables.Settings = {};
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.dtOptions = {
+			pagingType: 'full_numbers'
+		};
+	}
 
 }
