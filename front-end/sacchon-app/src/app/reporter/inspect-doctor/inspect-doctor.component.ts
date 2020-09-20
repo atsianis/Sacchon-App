1f4f16870613d0381f0a2a25cf9caa3e76587eef
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-20 23:30:34 +0300

    WIP: Implement mock api data for doctor

--- front-end/sacchon-app/src/app/reporter/inspect-doctor/inspect-doctor.component.ts
+++ front-end/sacchon-app/src/app/reporter/inspect-doctor/inspect-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Doctors } from 'src/app/interfaces/doctors';
+import { ReporterService } from '../reporter.service';
 
 @Component({
 	selector: 'sacchon-app-inspect-doctor',
@@ -7,12 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InspectDoctorComponent implements OnInit {
 
-	constructor() { }
+	doctors: [];
+
+	constructor(private reporterService: ReporterService) { }
 	dtOptions: DataTables.Settings = {};
 
 	ngOnInit(): void {
 		this.dtOptions = {
-			pagingType: 'full_numbers'
+			pagingType: 'full_numbers',
+			pageLength: 5,
+			order: [1, 'desc']
 		};
+		this.getDoctors();
+	}
+
+	getDoctors(): void {
+		this.reporterService.getDoctors().subscribe(doctors => {
+			this.doctors = doctors.results;
+			console.log(this.doctors);
+		});
 	}
 }
