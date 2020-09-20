2f8bb6d3f913a8f5d0c7e242d3552e4d144ee973
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-20 11:13:08 +0300

    Implement mock chart for user profile

--- front-end/sacchon-app/src/app/medi-data-repo/profile/profile.component.ts
+++ front-end/sacchon-app/src/app/medi-data-repo/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
 	selector: 'sacchon-app-profile',
@@ -8,6 +10,50 @@ import { Component, OnInit } from '@angular/core';
 export class PatientProfileComponent implements OnInit {
 
 	constructor() { }
+
+	// Array of different segments in chart
+	lineChartData: ChartDataSets[] = [
+		{ data: [65, 59, 80, 81, 56, 55, 40], label: 'Calories (gr)' },
+		{ data: [28, 48, 40, 19, 86, 27, 90], label: 'Glycose (ml)' }
+	];
+
+	//Labels shown on the x-axis
+	lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+	// Define chart options
+	lineChartOptions: ChartOptions = {
+		responsive: true
+	};
+
+	// Define colors of chart segments
+	lineChartColors: Color[] = [
+
+		{ // dark grey
+			backgroundColor: 'rgba(77,83,96,0.2)',
+			borderColor: 'rgba(77,83,96,1)',
+		},
+		{ // red
+			backgroundColor: 'rgba(255,0,0,0.3)',
+			borderColor: 'red',
+		}
+	];
+
+	// Set true to show legends
+	lineChartLegend = true;
+
+	// Define type of chart
+	lineChartType = 'line';
+
+	lineChartPlugins = [];
+
+	// events
+	chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+		console.log(event, active);
+	}
+
+	chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+		console.log(event, active);
+	}
 
 	ngOnInit(): void {
 	}
