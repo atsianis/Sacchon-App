fdb761bf6b32c9406b11cc37d4dbc895c07d1331
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-22 18:26:23 +0300

    Create functional patient profile for doctor

--- front-end/sacchon-app/src/app/doctor-advice/patient/patient-list/patient-list.component.ts
+++ front-end/sacchon-app/src/app/doctor-advice/patient/patient-list/patient-list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 
@@ -9,7 +11,8 @@ import { Color, Label } from 'ng2-charts';
 })
 export class PatientListComponent implements OnInit {
 
-	constructor() { }
+	constructor(private route: ActivatedRoute, private http: HttpClient) { }
+	patient: any;
 	// Array of different segments in chart
 	lineChartData: ChartDataSets[] = [
 		{ data: [65, 59, 80, 81, 56, 55, 40], label: 'Calories (gr)' },
@@ -54,8 +57,18 @@ export class PatientListComponent implements OnInit {
 		console.log(event, active);
 	}
 
+	getPatientById(): void {
+		this.route.params.subscribe(params => {
+			this.http.get(`https://jsonplaceholder.typicode.com/users/${params.id}`).subscribe(patient => {
+				this.patient = patient;
+			}, (err) => {
+				console.log('-----> err', err);
+			});
+		});
+	}
 
 	ngOnInit(): void {
+		this.getPatientById();
 	}
 
 }
