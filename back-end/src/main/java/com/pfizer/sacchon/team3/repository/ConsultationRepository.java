1db0681e80d634bca7df3d0ef609da612d91f60f
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-23 03:14:14 +0300

    add findPatientsConsultations method in Consultation Repository
    finds the consultations that refer to a specific patient

--- back-end/src/main/java/com/pfizer/sacchon/team3/repository/ConsultationRepository.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/repository/ConsultationRepository.java
@@ -1,8 +1,11 @@
 package com.pfizer.sacchon.team3.repository;
 
 import com.pfizer.sacchon.team3.model.Consultations;
+import org.hibernate.Hibernate;
 
 import javax.persistence.EntityManager;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +24,16 @@ public class ConsultationRepository {
 
     public List<Consultations> findAll() {
         return entityManager.createQuery("from Consultations").getResultList();
+    }
+
+    public List<Consultations> findPatientsConsultations(long patients_id){
+        List<BigInteger> consultationsIDs = entityManager.createNativeQuery("select distinct pr.consultation_id from PatientRecords as pr \n" +
+                "where pr.patient_id = "+patients_id+" ;").getResultList();
+
+        List<Consultations> consultations = new ArrayList<>();
+        consultationsIDs.forEach(c -> consultations.add(findById(c.longValue()).get()));
+        consultations.forEach(c-> Hibernate.initialize(c.getPatientRecords()));
+
+        return consultations;
     }
 }
