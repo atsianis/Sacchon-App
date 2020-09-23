292f3e85310340ee5bcb7f3aa73477ffb1cfc714
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-23 03:04:40 +0300

    refactor storeData method

--- back-end/src/main/java/com/pfizer/sacchon/team3/resource/patient/PatientRecordsListImpl.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/resource/patient/PatientRecordsListImpl.java
@@ -5,6 +5,7 @@ import com.pfizer.sacchon.team3.exception.NotFoundException;
 import com.pfizer.sacchon.team3.model.PatientRecords;
 import com.pfizer.sacchon.team3.model.Patients;
 import com.pfizer.sacchon.team3.repository.PatientRecordRepository;
+import com.pfizer.sacchon.team3.repository.PatientRepository;
 import com.pfizer.sacchon.team3.repository.util.JpaUtil;
 import com.pfizer.sacchon.team3.representation.PatientRecordRepresentation;
 import com.pfizer.sacchon.team3.representation.PatientRepresentation;
@@ -25,12 +26,16 @@ import java.util.logging.Logger;
 public class PatientRecordsListImpl extends ServerResource implements PatientRecordsList {
     public static final Logger LOGGER = Engine.getLogger(PatientRecordsListImpl.class);
     private PatientRecordRepository patientRecordRepository;
+    private PatientRepository patientRepository;
+    long id;
 
     @Override
     protected void doInit() {
         LOGGER.info("Initialising patient record resource L starts");
         try {
             patientRecordRepository = new PatientRecordRepository(JpaUtil.getEntityManager());
+            patientRepository = new PatientRepository(JpaUtil.getEntityManager());
+            id = Long.parseLong(getAttribute("id"));
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -59,50 +64,56 @@ public class PatientRecordsListImpl extends ServerResource implements PatientRec
     }
 
     @Override
-    public PatientRecordRepresentation storeData(PatientRecordRepresentation patientRecordRepresentation, Patients patient) throws NotFoundException, BadEntityException {
+    public PatientRecordRepresentation storeData(PatientRecordRepresentation patientRecordRepresentation) throws NotFoundException, BadEntityException {
         LOGGER.finer("Add a new record.");
         // Check authorization
         ResourceUtils.checkRole(this, Shield.ROLE_PATIENT);
         LOGGER.finer("User allowed to add a record.");
-        // Check entity
-        // Convert to PatientRepr so a validation can procceed
-        PatientRepresentation pr = new PatientRepresentation();
-        pr.setLastName(patient.getLastName());
-        pr.setFirstName(patient.getFirstName());
+        // get patient
+        Optional<Patients> opatient = patientRepository.findById(id);
+        if (opatient.isPresent()){
+            // Check entity
+            // Convert to PatientRepr so a validation can procceed
+            Patients patient = opatient.get();
+            PatientRepresentation pr = new PatientRepresentation();
+            pr.setLastName(patient.getLastName());
+            pr.setFirstName(patient.getFirstName());
+            ResourceValidator.notNull(patientRecordRepresentation);
+            ResourceValidator.validatePatient(pr);
+            LOGGER.finer("Patient checked");
 
-        ResourceValidator.notNull(patientRecordRepresentation);
-        ResourceValidator.validatePatient(pr);
-        LOGGER.finer("Patient checked");
+            try {
+                // Convert PatientRecordRepr to PatientRecord
+                PatientRecords patientRecordsIn = new PatientRecords();
+                patientRecordsIn.setSacchon(patientRecordRepresentation.getSacchon());
+                patientRecordsIn.setCalories(patientRecordRepresentation.getCalories());
+                patientRecordsIn.setTimeCreated(patientRecordRepresentation.getTimeCreated());
+                patientRecordsIn.setPatient(patient);
 
-        try {
-            // Convert PatientRecordRepr to PatientRecord
-            PatientRecords patientRecordsIn = new PatientRecords();
-            patientRecordsIn.setSacchon(patientRecordRepresentation.getSacchon());
-            patientRecordsIn.setCalories(patientRecordRepresentation.getCalories());
-            patientRecordsIn.setTimeCreated(patientRecordRepresentation.getTimeCreated());
-            patientRecordsIn.setPatient(patient);
+                Optional<PatientRecords> patientRecordsOut = patientRecordRepository.save(patientRecordsIn);
+                PatientRecords patientRecords = null;
+                if(patientRecordsOut.isPresent())
+                    patientRecords = patientRecordsOut.get();
+                else
+                    throw new BadEntityException("Record has not been created");
 
-            Optional<PatientRecords> patientRecordsOut = patientRecordRepository.save(patientRecordsIn);
-            PatientRecords patientRecords = null;
-            if(patientRecordsOut.isPresent())
-                patientRecords = patientRecordsOut.get();
-            else
-                throw new BadEntityException("Record has not been created");
+                PatientRecordRepresentation result = new PatientRecordRepresentation();
+                result.setSacchon(patientRecords.getSacchon());
+                result.setCalories(patientRecords.getCalories());
+                result.setTimeCreated(patientRecords.getTimeCreated());
+                result.setUri("/patient/"+patient.getId()+"/storeData/patientRecord/"+patientRecords.getId());
 
-            PatientRecordRepresentation result = new PatientRecordRepresentation();
-            result.setSacchon(patientRecords.getSacchon());
-            result.setCalories(patientRecords.getCalories());
-            result.setTimeCreated(patientRecords.getTimeCreated());
-            result.setUri("/patient/"+patient.getId()+"/storeData/patientRecord/"+patientRecords.getId());
+                getResponse().setLocationRef("http://localhost:9000/v1/patient/"+patient.getId()+"/storeData/patientRecord/"+patientRecords.getId());
+                getResponse().setStatus(Status.SUCCESS_CREATED);
 
-            getResponse().setLocationRef("http://localhost:9000/v1/patient/"+patient.getId()+"/storeData/patientRecord/"+patientRecords.getId());
-            getResponse().setStatus(Status.SUCCESS_CREATED);
-
-            LOGGER.finer("Record successfully added.");
-            return result;
-        } catch (Exception ex) {
-            LOGGER.log(Level.WARNING, "Error when adding a Record", ex);
-            throw new ResourceException(ex);
+                LOGGER.finer("Record successfully added.");
+                return result;
+            } catch (Exception ex) {
+                LOGGER.log(Level.WARNING, "Error when adding a Record", ex);
+                throw new ResourceException(ex);
+            }
+        }else{
+            throw new NotFoundException("Patient not found");
         }
     }
 }
