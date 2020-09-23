1d8e542e8783d415ccdf15f8ab8699586edbf7ad
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-23 03:04:08 +0300

    correct addDoctor signature in the RegisterDoctor interface

--- back-end/src/main/java/com/pfizer/sacchon/team3/resource/register/RegisterDoctor.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/resource/register/RegisterDoctor.java
@@ -1,10 +1,11 @@
 package com.pfizer.sacchon.team3.resource.register;
 
 import com.pfizer.sacchon.team3.exception.BadEntityException;
+import com.pfizer.sacchon.team3.representation.CreatedDoctorRepresentation;
 import com.pfizer.sacchon.team3.representation.DoctorRepresentation;
 import org.restlet.resource.Post;
 
 public interface RegisterDoctor {
     @Post("json")
-    public DoctorRepresentation add(DoctorRepresentation doctorRepresentation) throws BadEntityException;
+    public DoctorRepresentation add(CreatedDoctorRepresentation doctorRepresentation) throws BadEntityException;
 }
