41390ddd909c8a656a2c49efa64b47416899f9eb
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-24 10:52:11 +0300

    Lint code

--- back-end/src/main/java/com/pfizer/sacchon/team3/security/dao/ApplicationUserPersistence.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/security/dao/ApplicationUserPersistence.java
@@ -12,19 +12,18 @@ public class ApplicationUserPersistence   {
     private static ApplicationUserPersistence applicationUserPersistence = new ApplicationUserPersistence();
     private ApplicationUserPersistence() {
     }
+
     public static synchronized ApplicationUserPersistence getApplicationUserPersistence() {
         return applicationUserPersistence;
     }
 
      public ApplicationUser findById(String email) throws SQLException {
-        Context.getCurrentLogger().finer(
-                "Method findById() of ApplicationUserPersistence called.");
+        Context.getCurrentLogger().finer("Method findById() of ApplicationUserPersistence called.");
 
         Connection connection = null;
         try {
             connection = getConnection();
-            PreparedStatement preparedStatement = connection
-                    .prepareStatement("select * from UserTable where email=?");
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from UserTable where email=?");
             preparedStatement.setString(1, email);
             ResultSet rs = preparedStatement.executeQuery();
 
@@ -38,11 +37,9 @@ public class ApplicationUserPersistence   {
             return null;
         } finally {
             releaseConnection(connection);
-            Context.getCurrentLogger().finer(
-                    "Method findById() of CompanyPersistence finished.");
+            Context.getCurrentLogger().finer("Method findById() of CompanyPersistence finished.");
         }
     }
-
 
     protected Connection getConnection() throws SQLException {
         Context.getCurrentLogger().finer("Get a fresh connection to database");
