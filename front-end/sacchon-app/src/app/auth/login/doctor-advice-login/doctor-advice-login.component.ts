7671fe485bd87026b05ca5336593c8038739409e
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-24 12:32:41 +0300

    Add login component for doctor advice

---
+++ front-end/sacchon-app/src/app/auth/login/doctor-advice-login/doctor-advice-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-doctor-advice-login',
+  templateUrl: './doctor-advice-login.component.html',
+  styleUrls: ['./doctor-advice-login.component.scss']
+})
+export class DoctorAdviceLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
