d74d014ca699c909280c3dfcab6710749d37c676
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-24 12:33:02 +0300

    add component for medidatarepo login

---
+++ front-end/sacchon-app/src/app/auth/login/medi-data-repo-login/medi-data-repo-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-medi-data-repo-login',
+  templateUrl: './medi-data-repo-login.component.html',
+  styleUrls: ['./medi-data-repo-login.component.scss']
+})
+export class MediDataRepoLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
