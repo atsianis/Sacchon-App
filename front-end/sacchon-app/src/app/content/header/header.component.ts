8c248f7f97896887bf89cdbebc194a60ada3ae67
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-24 22:43:37 +0300

    Add logic for sign in/out

--- front-end/sacchon-app/src/app/content/header/header.component.ts
+++ front-end/sacchon-app/src/app/content/header/header.component.ts
@@ -9,7 +9,22 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  user: string = null;
+
   ngOnInit(): void {
+    this.isLoggedIn()
+  }
+
+  isLoggedIn(): boolean {
+    this.user = sessionStorage.getItem('email')
+    console.log(this.user !== null)
+      return this.user !== null 
+  }
+
+  signOut(): any {
+    sessionStorage.removeItem('email')
+    sessionStorage.removeItem('password')
+    location.reload();
   }
 
 }
