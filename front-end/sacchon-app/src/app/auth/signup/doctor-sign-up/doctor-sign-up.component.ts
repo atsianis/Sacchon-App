b3c2d39212c3d90c1c75aa2742526c3a0681f1c4
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-26 02:35:06 +0300

    WIP: progress with toastr and validation

--- front-end/sacchon-app/src/app/auth/signup/doctor-sign-up/doctor-sign-up.component.ts
+++ front-end/sacchon-app/src/app/auth/signup/doctor-sign-up/doctor-sign-up.component.ts
@@ -5,6 +5,7 @@ import {
 	ValidationErrors,
 	Validators,
 } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'sacchon-app-doctor-sign-up',
@@ -12,44 +13,38 @@ import {
 	styleUrls: ['./doctor-sign-up.component.scss'],
 })
 export class DoctorSignUpComponent implements OnInit {
-	constructor() {}
+	constructor(private toastr: ToastrService) { }
 
 	doctorSignUp = new FormGroup({
 		firstName: new FormControl(null, [Validators.required]),
 		lastName: new FormControl(null, [Validators.required]),
-		email: new FormControl(null, [Validators.required]),
-		password: new FormControl(null, [Validators.required]),
-		passwordconfirm: new FormControl(null, [Validators.required]),
+		email: new FormControl(null, [Validators.required, Validators.email]),
+		password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+		passwordconfirm: new FormControl(null)
 	});
 
-	ngOnInit(): void {}
+	ngOnInit(): void { }
 
-	signUp() {
+	signUp(): void {
 		if (this.doctorSignUp.valid) {
-			if (
-				this.doctorSignUp.get('password').value ===
-				this.doctorSignUp.get('passwordconfirm').value
-			) {
+			if (this.doctorSignUp.get('password').value === this.doctorSignUp.get('passwordconfirm').value) {
 				console.log(this.doctorSignUp.get('password').value);
 				console.log('EO', this.doctorSignUp.value);
+				this.toastr.success('You will be redirected to home page soon.', 'Successfully registered', {
+					timeOut: 2000,
+					positionClass: 'toast-top-center'
+				}).onHidden.toPromise().then(_ => {
+					location.href = '/doctoradvice/profile';
+				});
 			} else {
-				console.log('pouli');
+				this.toastr.error('Passwords do not match.')
 			}
 		} else {
 			Object.keys(this.doctorSignUp.controls).forEach((key) => {
-				const controlErrors: ValidationErrors = this.doctorSignUp.get(
-					key
-				).errors;
+				const controlErrors: ValidationErrors = this.doctorSignUp.get(key).errors;
 				if (controlErrors != null) {
 					Object.keys(controlErrors).forEach((keyError) => {
-						console.log(
-							'Key control: ' +
-								key +
-								', keyError: ' +
-								keyError +
-								', err value: ',
-							controlErrors[keyError]
-						);
+						this.toastr.error(`The field ${key} is ${keyError}`);
 					});
 				}
 			});
