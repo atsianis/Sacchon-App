4705dcbb8f2553d667e64b079f6cce1dec672fa9
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-25 18:15:08 +0300

    Add doctor sign up component

---
+++ front-end/sacchon-app/src/app/auth/signup/doctor-sign-up/doctor-sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-doctor-sign-up',
+  templateUrl: './doctor-sign-up.component.html',
+  styleUrls: ['./doctor-sign-up.component.scss']
+})
+export class DoctorSignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
