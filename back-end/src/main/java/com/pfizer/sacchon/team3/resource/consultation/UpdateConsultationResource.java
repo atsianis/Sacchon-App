ab2eba0e4f502665295bae334f989ea61cd7674b
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-26 20:17:19 +0300

    change updateConsultation implementation

--- back-end/src/main/java/com/pfizer/sacchon/team3/resource/consultation/UpdateConsultationResource.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/resource/consultation/UpdateConsultationResource.java
@@ -5,9 +5,11 @@ import com.pfizer.sacchon.team3.exception.NotFoundException;
 import com.pfizer.sacchon.team3.model.Consultations;
 import com.pfizer.sacchon.team3.model.Doctors;
 import com.pfizer.sacchon.team3.repository.ConsultationRepository;
+import com.pfizer.sacchon.team3.repository.DoctorRepository;
 import com.pfizer.sacchon.team3.repository.util.JpaUtil;
 import com.pfizer.sacchon.team3.representation.ConsultationRepresentation;
 import com.pfizer.sacchon.team3.representation.DoctorRepresentation;
+import com.pfizer.sacchon.team3.representation.UpdateConsultationRepresentation;
 import com.pfizer.sacchon.team3.resource.doctor.DoctorResourceImpl;
 import com.pfizer.sacchon.team3.resource.util.ResourceValidator;
 import com.pfizer.sacchon.team3.security.ResourceUtils;
@@ -25,15 +27,17 @@ public class UpdateConsultationResource extends ServerResource implements Update
 
     public static final Logger LOGGER = Engine.getLogger(DoctorResourceImpl.class);
 
-    private long id;
+    private long doctor_id;
+    private long consultation_id;
     private ConsultationRepository consultationRepository ;
+    private DoctorRepository doctorRepository ;
     private ConsultationRepresentation consultationRepresentation;
     private EntityManager em;
 
-    @Override
-    protected void doRelease(){
-        em.close();
-    }
+//    @Override
+//    protected void doRelease(){
+//        em.close();
+//    }
 
     @Override
     protected void doInit() {
@@ -41,65 +45,67 @@ public class UpdateConsultationResource extends ServerResource implements Update
         try {
             consultationRepository = new ConsultationRepository(JpaUtil.getEntityManager());
             consultationRepresentation = new ConsultationRepresentation();
-            id = Long.parseLong(getAttribute("id"));
+            doctorRepository = new DoctorRepository(JpaUtil.getEntityManager());
+            doctor_id = Long.parseLong(getAttribute("did"));
+            consultation_id = Long.parseLong(getAttribute("cid"));
         } catch (Exception e) {
-            id = -1;
+            consultation_id = -1;
+            doctor_id = -1;
         }
         LOGGER.info("Initialising doctor resource ends");
     }
 
-    public ConsultationRepresentation UpdateConsultation(ConsultationRepresentation consultReprIn, Doctors doctor) throws NotFoundException, BadEntityException {
+    public ConsultationRepresentation updateConsultation(UpdateConsultationRepresentation consultReprIn) throws NotFoundException, BadEntityException {
 
         LOGGER.finer("Update a consultation.");
 
         ResourceUtils.checkRole(this, Shield.ROLE_DOCTOR);
         LOGGER.finer("User allowed to update a consultation.");
-        // Check given entity
-        // Convert to PatientRepr so a validation can procceed
-        DoctorRepresentation dr = new DoctorRepresentation();
-        dr.setLastName(doctor.getLastName());
-        dr.setFirstName(doctor.getFirstName());
-
-        ResourceValidator.notNull(consultReprIn);
-        ResourceValidator.validateDoctor(dr);
-        LOGGER.finer("Doctor checked");
-
+        // get Doctor
+        Optional<Doctors> odoctor = doctorRepository.findById(doctor_id);
+        Doctors doctor;
+        setExisting(odoctor.isPresent());
+        if (!isExisting()) {
+            LOGGER.config("Doctor id does not exist:" + doctor_id);
+            throw new NotFoundException("No doctor with id  : " + doctor_id);
+        } else {
+            doctor = odoctor.get();
+            LOGGER.finer("Doctor allowed to update a consultation.");
+        }
+        // We have the doctor
+        // Now we find the consultation
         try {
-            // Convert PatientRecordRepr to PatientRecord
-            Consultations consultationsIn = consultReprIn.createConsultation();
-            consultationsIn.setId(id);
-            Optional<Consultations> consultationOut = consultationRepository.findById(id);
+            Optional<Consultations> consultationOut = consultationRepository.findById(consultation_id);
+            Consultations consultation;
             setExisting(consultationOut.isPresent());
             // If patientRecord exists, we update it.
             if (isExisting()) {
+                consultation= consultationOut.get();
+                if (!isMyConsult(doctor,consultation)){
+                    throw new NotFoundException("This consult is not yours");
+                }
+                consultation.setComment(consultReprIn.getComment());
                 LOGGER.finer("Update consultation.");
                 // Update Record in DB and retrieve the new one.
 
-                consultationOut = consultationRepository.setComment(consultationsIn);
-
-                // Create new Consultation with initialized patient_id and Date
-                Consultations consultation = consultationRepresentation.createConsultation();
-                consultation.setPatient(consultReprIn.getPatient());
-                consultation.setTimeCreated(new Date());
-                // Add consult in db
-                consultationRepository.save(consultation);
-
-                // Check if retrieved patient is not null : if it is null it
-                // means that the id is wrong.
-                if (!consultationOut.isPresent()) {
-                    LOGGER.finer("Consultation does not exist.");
-                    throw new NotFoundException("Consultation with the following id does not exist: " + id);
+                Optional<Consultations> oconsult = consultationRepository.setComment(consultation);
+                setExisting(oconsult.isPresent());
+                if(isExisting()){
+                    return new ConsultationRepresentation(oconsult.get());
+                }else{
+                    throw new NotFoundException("consultation is not found");
                 }
+
             } else {
                 LOGGER.finer("Resource does not exist.");
-                throw new NotFoundException("Company with the following id does not exist: " + id);
+                throw new NotFoundException("Consultation with the following id does not exist: " + consultation_id);
             }
-
-            LOGGER.finer("Company successfully updated.");
-
-            return new ConsultationRepresentation(consultationOut.get());
         } catch (Exception ex) {
             throw new ResourceException(ex);
         }
+    }
+
+    private boolean isMyConsult(Doctors d, Consultations c){
+        return d.getId()==c.getDoctor().getId();
     }
 }
