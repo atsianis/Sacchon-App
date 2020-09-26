f630fc1308af3f9a6c6382b97e18f49bda8bbaa3
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-26 18:12:37 +0300

    Properly validate patient sign up

--- front-end/sacchon-app/src/app/auth/signup/patient-sign-up/patient-sign-up.component.ts
+++ front-end/sacchon-app/src/app/auth/signup/patient-sign-up/patient-sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'sacchon-app-patient-sign-up',
@@ -7,46 +8,30 @@ import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/f
 	styleUrls: ['./patient-sign-up.component.scss'],
 })
 export class PatientSignUpComponent implements OnInit {
-	constructor() {}
+	constructor(private toastr: ToastrService) { }
 
 	patientSignUp = new FormGroup({
 		firstName: new FormControl(null, [Validators.required]),
 		lastName: new FormControl(null, [Validators.required]),
-		email: new FormControl(null, [Validators.required]),
-		password: new FormControl(null, [Validators.required]),
+		email: new FormControl(null, [Validators.required, Validators.email]),
+		password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
 		passwordconfirm: new FormControl(null, [Validators.required]),
 	});
 
-	ngOnInit(): void {}
-	signUp() {
-		if (this.patientSignUp.valid) {
-			if (
-				this.patientSignUp.get('password').value ===
-				this.patientSignUp.get('passwordconfirm').value
-			) {
-				console.log(this.patientSignUp.get('password').value);
-				console.log('EO', this.patientSignUp.value);
-			} else {
-				console.log('pouli');
-			}
-		} else {
-			Object.keys(this.patientSignUp.controls).forEach((key) => {
-				const controlErrors: ValidationErrors = this.patientSignUp.get(
-					key
-				).errors;
-				if (controlErrors != null) {
-					Object.keys(controlErrors).forEach((keyError) => {
-						console.log(
-							'Key control: ' +
-								key +
-								', keyError: ' +
-								keyError +
-								', err value: ',
-							controlErrors[keyError]
-						);
-					});
-				}
+	ngOnInit(): void { }
+
+	signUp(): void {
+		if (this.patientSignUp.valid && (this.patientSignUp.get('password').value === this.patientSignUp.get('passwordconfirm').value)) {
+			console.log(this.patientSignUp.get('password').value);
+			console.log('EO', this.patientSignUp.value);
+			this.toastr.success('You will be redirected to home page soon.', 'Successfully registered', {
+				timeOut: 2000,
+				positionClass: 'toast-top-center'
+			}).onHidden.toPromise().then(_ => {
+				location.href = '/doctoradvice/profile';
 			});
+		} else {
+			this.patientSignUp.markAllAsTouched();
 		}
 	}
 }
