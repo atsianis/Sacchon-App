5d66074de424afb8d6630235d1c767a0c3f969fa
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-26 11:46:47 +0300

    rename patientRecords endpoint

--- back-end/src/main/java/com/pfizer/sacchon/team3/router/CustomRouter.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/router/CustomRouter.java
@@ -5,7 +5,7 @@ import com.pfizer.sacchon.team3.resource.chief.AllDoctorsListImpl;
 import com.pfizer.sacchon.team3.resource.consultation.ConsultationListResourceImpl;
 import com.pfizer.sacchon.team3.resource.consultation.ConsultationResourceImpl;
 import com.pfizer.sacchon.team3.resource.doctor.AllAvailablePatientListResourceImpl;
-import com.pfizer.sacchon.team3.resource.doctor.AvailablePatientsFromToResourceImpl;
+import com.pfizer.sacchon.team3.resource.doctor.AllConsultablePatientListResourceImpl;
 import com.pfizer.sacchon.team3.resource.doctor.DoctorResourceImpl;
 import com.pfizer.sacchon.team3.resource.doctor.MyPatientsResourceImpl;
 import com.pfizer.sacchon.team3.resource.PingServerResource;
@@ -13,7 +13,6 @@ import com.pfizer.sacchon.team3.resource.patient.PatientConsultationsResourceImp
 import com.pfizer.sacchon.team3.resource.patient.PatientRecordsListImpl;
 import com.pfizer.sacchon.team3.resource.patient.PatientResourceImpl;
 import com.pfizer.sacchon.team3.resource.patientRecord.PatientRecordResourceImpl;
-import com.pfizer.sacchon.team3.resource.userAuth.register.RegisterPatientImpl;
 import com.pfizer.sacchon.team3.resource.userAuth.register.RegisterDoctorImpl;
 import org.restlet.Application;
 import org.restlet.routing.Router;
@@ -54,8 +53,13 @@ public class CustomRouter {
         // Doctor's patients
         router.attach("/doctor/{id}/mypatients",  MyPatientsResourceImpl.class);
 
-        // Available patients
-        router.attach("/doctors/available",  AllAvailablePatientListResourceImpl.class);
+        // Consultable patients (doctor_id = null && canBeExamined  = true)
+        router.attach("/doctors/consultable-patients",  AllConsultablePatientListResourceImpl.class);
+
+        // Available patients (doctor_id = null && canBeExamined  = false)
+        router.attach("/doctors/available-patients",  AllAvailablePatientListResourceImpl.class);
+
+        router.attach("/doctors/available-patients",)
 
         // Get Patients Consults
         router.attach("/patient/{id}/consultations", PatientConsultationsResourceImpl.class);
@@ -67,7 +71,7 @@ public class CustomRouter {
         router.attach("/patient/{id}/settings", PatientResourceImpl.class);
         router.attach("/patients/{id}/storeData", PatientRecordsListImpl.class);
         router.attach("/patients/{id}/storeData/allData", PatientRecordsListImpl.class);
-        router.attach("/patient/{id}/storeData/patientRecord/{id}", PatientRecordResourceImpl.class);
+        router.attach("/patient/{pid}/storeData/patientRecord/{rid}", PatientRecordResourceImpl.class);
 
         return router;
     }
