dd67314bb7f6bbca2a89a03b9732fe350d328bd7
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-27 16:52:45 +0300

    Refactor login functionality

--- front-end/sacchon-app/src/app/auth/login/reporter-login/reporter-login.component.ts
+++ front-end/sacchon-app/src/app/auth/login/reporter-login/reporter-login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Chiefs } from 'src/app/interfaces/chiefs';
 
 @Component({
 	selector: 'sacchon-app-reporter-login',
@@ -8,28 +10,43 @@ import { FormBuilder } from '@angular/forms';
 })
 export class ReporterLoginComponent implements OnInit {
 
-	reporterLoginForm: any;
-	constructor(private formBuilder: FormBuilder) {
-		this.reporterLoginForm = this.formBuilder.group({
-			email: '',
-			password: ''
-		});
-	}
+	constructor(private formBuilder: FormBuilder, private http: HttpClient) {	}
+
+	reporterLoginForm: any = this.formBuilder.group({
+		email: '',
+		password: ''
+	});
 
 	ngOnInit(): void {
 	}
 
-	submitLogin(data: any): any {
-		sessionStorage.setItem('email', data.email);
-		sessionStorage.setItem('password', data.password);
-		if (1) {
-			window.location.href = 'http://localhost:4200/reporter';
-		} else {
-			alert('Invalid username or password');
-			this.reporterLoginForm = this.formBuilder.group({
-				email: '',
-				password: ''
-			});
-		}
+	httpOptions = {
+		headers: new HttpHeaders({
+			'Authorization': 'Basic ' + btoa('asd@asd.asd:asdasdasd'),
+			'Access-Control-Allow-Origin': '*'
+		})
+	};
+
+	submitLogin(): any {
+		this.http.post<Chiefs>('http://localhost:9000/v1/login/chief', {
+			email: this.reporterLoginForm.get('email').value,
+			password: this.reporterLoginForm.get('password').value
+		}, this.httpOptions).subscribe(response => {
+			console.log(response);
+			if (response) {
+				sessionStorage.setItem('email', response?.email );
+				sessionStorage.setItem('password', response?.password );
+				sessionStorage.setItem('firstName', response?.firstName );
+				sessionStorage.setItem('lastName', response?.lastName );
+				sessionStorage.setItem('id', response?.id );
+				window.location.href = 'http://localhost:4200/reporter';
+			} else {
+				alert('Invalid username or password');
+				this.reporterLoginForm = this.formBuilder.group({
+					email: '',
+					password: ''
+				});
+			}
+		})
 	}
 }
