11cf27bcb1f502fc1f63ce9ea666c95166cfc33b
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-27 23:28:46 +0300

    Remove unused imports

--- front-end/sacchon-app/src/app/app-routing.module.ts
+++ front-end/sacchon-app/src/app/app-routing.module.ts
@@ -6,9 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { MediDataRepoLoginComponent } from './auth/login/medi-data-repo-login/medi-data-repo-login.component';
 import { DoctorAdviceLoginComponent } from './auth/login/doctor-advice-login/doctor-advice-login.component';
 import { ReporterLoginComponent } from './auth/login/reporter-login/reporter-login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { ContentComponent } from './content/content.component';
-import { DoctorSignUpComponent } from './auth/signup/doctor-sign-up/doctor-sign-up.component';
 import { PatientSignUpComponent } from './auth/signup/patient-sign-up/patient-sign-up.component';
 
 const routes: Routes = [
