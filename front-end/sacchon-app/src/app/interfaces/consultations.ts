e4f47c896e1ffc48f37dd07248650c5107583098
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-28 19:50:04 +0300

    Add proper interface

--- front-end/sacchon-app/src/app/interfaces/consultations.ts
+++ front-end/sacchon-app/src/app/interfaces/consultations.ts
@@ -1,2 +1,11 @@
 export interface Consultations {
+	status: number;
+	data: {
+		comment: string;
+		doctor: {};
+		id: number;
+		patient: {};
+		seenByPatient: number;
+		timeCreated: number;
+	}
 }
