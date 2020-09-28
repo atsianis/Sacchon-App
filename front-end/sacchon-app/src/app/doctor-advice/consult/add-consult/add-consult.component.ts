28b914246a700481eee6fa0acffb07fbab9b1d54
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-28 17:38:24 +0300

    Refactor consult add edit and list component

---
+++ front-end/sacchon-app/src/app/doctor-advice/consult/add-consult/add-consult.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sacchon-app-add-consult',
+  templateUrl: './add-consult.component.html',
+  styleUrls: ['./add-consult.component.scss']
+})
+export class AddConsultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
