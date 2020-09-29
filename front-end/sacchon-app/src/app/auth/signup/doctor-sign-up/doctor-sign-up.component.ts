4cdab25b7efe09fcdf7b9c015531380fd10a810f
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-29 18:30:29 +0300

    Remove unused import

--- front-end/sacchon-app/src/app/auth/signup/doctor-sign-up/doctor-sign-up.component.ts
+++ front-end/sacchon-app/src/app/auth/signup/doctor-sign-up/doctor-sign-up.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
