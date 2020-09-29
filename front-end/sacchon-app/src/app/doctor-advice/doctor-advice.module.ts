bd9a1055c6a1d2d079883641be6b4579ec2e3795
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-29 17:02:39 +0300

    Remove consult list component

--- front-end/sacchon-app/src/app/doctor-advice/doctor-advice.module.ts
+++ front-end/sacchon-app/src/app/doctor-advice/doctor-advice.module.ts
@@ -11,7 +11,6 @@ import { PatientListComponent } from './patient/patient-list/patient-list.compon
 import { ConsultComponent } from './consult/consult.component';
 import { ConsultEditComponent } from './consult/consult-edit/consult-edit.component';
 import { AddConsultComponent } from './consult/add-consult/add-consult.component';
-import { ConsultListComponent } from './consult/consult-list/consult-list.component';
 import { DoctorAdviceGuard } from './doctor-advice.guard';
 
 
@@ -37,7 +36,7 @@ import { DoctorAdviceGuard } from './doctor-advice.guard';
 			{ path: 'doctoradvice/patients', component: PatientComponent, canActivate: [ DoctorAdviceGuard ] },
 			{ path: 'doctoradvice/patient/:id', component: PatientListComponent, canActivate: [ DoctorAdviceGuard ] },
 			{ path: 'doctoradvice/consult/:patient_id', component: ConsultComponent, canActivate: [ DoctorAdviceGuard ] },
-			{ path: 'doctoradvice/consult/:patient_id/add', component: ConsultListComponent, canActivate: [ DoctorAdviceGuard ] },
+			{ path: 'doctoradvice/consult/:patient_id/add', component: AddConsultComponent, canActivate: [ DoctorAdviceGuard ] },
 			{ path: 'doctoradvice/consult/:patient_id/:consultation_id/edit', component: ConsultEditComponent, canActivate: [ DoctorAdviceGuard ] },
 		])
 	],
