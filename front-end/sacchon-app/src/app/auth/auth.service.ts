126cfb6271dfea2e906bbc1e35650375027147fb
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-29 23:28:57 +0300

    Transfer all http methods from auth to its service

--- front-end/sacchon-app/src/app/auth/auth.service.ts
+++ front-end/sacchon-app/src/app/auth/auth.service.ts
@@ -1,16 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Chiefs } from '../interfaces/chiefs';
+import { Doctors } from '../interfaces/doctors';
+import { Patients } from '../interfaces/patients';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
 
-	constructor() { }
+	constructor(private http: HttpClient) { }
 
 	isLoggedIn(): boolean {
 		if (sessionStorage.getItem('userType'))
 			return true
-			
+
 		return false
+	}
+
+	doctorLogin(email, password): Observable<any> {
+		return this.http.post<any>('http://localhost:9000/v1/login/doctor', {
+			email: email,
+			password: password
+		})
+	}
+	
+	patientLogin(email, password): Observable<any> {
+		return this.http.post<any>('http://localhost:9000/v1/login/patient', {
+			email: email,
+			password: password
+		})
+	}
+
+	reporterLogin(email, password): Observable<any> {
+		return this.http.post<any>('http://localhost:9000/v1/login/reporter', {
+			email: email,
+			password: password
+		})
+	}
+
+	signUpDoctor(firstName, lastName, email, password): Observable<any> {
+		return this.http.post(`http://localhost:9000/v1/create/doctor`, {
+				firstName: firstName,
+				lastName: lastName,
+				email: email,
+				password: password,
+		})
 	}
 }
--- front-end/sacchon-app/src/app/auth/login/doctor-advice-login/doctor-advice-login.component.ts
+++ front-end/sacchon-app/src/app/auth/login/doctor-advice-login/doctor-advice-login.component.ts
@@ -1,10 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { report } from 'process';
-import { Doctors } from 'src/app/interfaces/doctors';
+import { AuthService } from '../../auth.service';
 
 @Component({
 	selector: 'sacchon-app-doctor-advice-login',
@@ -13,43 +11,46 @@ import { Doctors } from 'src/app/interfaces/doctors';
 })
 export class DoctorAdviceLoginComponent implements OnInit {
 
-	constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) {	}
+	constructor(private router: Router, private toastr: ToastrService, private authService: AuthService) { }
 
 	doctorLoginForm = new FormGroup({
 		email: new FormControl(null, [Validators.required, Validators.email]),
 		password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
 	});
 
-	doctor: any;
-
 	ngOnInit(): void {
 	}
 
-	submitLogin(): any {
-		this.http.post<Doctors>('http://localhost:9000/v1/login/doctor', {
-			email: this.doctorLoginForm.get('email').value,
-			password: this.doctorLoginForm.get('password').value
-		}).subscribe(response => {
-			if (response.status == 200) {
-				this.toastr.success(`Welcome ${response.data[0].firstName}!`, 'Login successful', {
-					timeOut: 2000,
-					positionClass: 'toast-top-center'
-				}).onHidden.toPromise().then(_ => {
-					sessionStorage.setItem('email', response.data[0].email);
-					sessionStorage.setItem('password', response.data[0].password);
-					sessionStorage.setItem('firstName', response.data[0].firstName);
-					sessionStorage.setItem('lastName', response.data[0].lastName);
-					sessionStorage.setItem('id', response.data[0].id);
-					sessionStorage.setItem('lastActive', response.data[0].lastActive);
-					sessionStorage.setItem('userType', 'doctor');
-					this.router.navigate(['doctoradvice/profile']);
-				})
-			} else {
-				this.toastr.error('Invalid credentials', 'Login Unsuccessful', {
-					timeOut: 2000,
-					positionClass: 'toast-top-center'
-				})
-			}
+	submitLogin(): void {
+		const email = this.doctorLoginForm.get('email').value
+		const password = this.doctorLoginForm.get('password').value
+
+		this.authService.doctorLogin(email, password).subscribe(response => {
+			console.log(response)
+			this.handleLogin(response)
 		})
+	}
+
+	handleLogin(response): void {
+		if (response.status == 200) {
+			this.toastr.success(`Welcome ${response.data.firstName}!`, 'Login successful', {
+				timeOut: 2000,
+				positionClass: 'toast-top-center'
+			}).onHidden.toPromise().then(_ => {
+				sessionStorage.setItem('email', response.data.email);
+				sessionStorage.setItem('password', response.data.password);
+				sessionStorage.setItem('firstName', response.data.firstName);
+				sessionStorage.setItem('lastName', response.data.lastName);
+				sessionStorage.setItem('id', response.data.id);
+				sessionStorage.setItem('lastActive', response.data.lastActive);
+				sessionStorage.setItem('userType', 'doctor');
+				this.router.navigate(['doctoradvice/profile']);
+			})
+		} else {
+			this.toastr.error('Invalid credentials', 'Login Unsuccessful', {
+				timeOut: 2000,
+				positionClass: 'toast-top-center'
+			})
+		}
 	}
 }
