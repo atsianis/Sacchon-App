0e94c67976b7589c1dbfb116e6fa136604eba7c9
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-30 20:27:27 +0300

    Implement edit patient record

--- front-end/sacchon-app/src/app/medi-data-repo/edit-patient-record/edit-patient-record.component.ts
+++ front-end/sacchon-app/src/app/medi-data-repo/edit-patient-record/edit-patient-record.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientRecords } from 'src/app/interfaces/patient-records';
+import { MediDataRepoService } from '../medi-data-repo.service';
+import * as moment from 'moment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'sacchon-app-edit-patient-record',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPatientRecordComponent implements OnInit {
 
-	constructor() { }
+	constructor(private patientService: MediDataRepoService, private route: ActivatedRoute, private toastr: ToastrService, private router: Router) { }
+
+	patientRecord: PatientRecords;
+	patient_id: string = sessionStorage.getItem('id');
+	record_id: string; 
 
 	ngOnInit(): void {
+		this.getCurrentRecord()
+	}
+
+	editPatientRecord: FormGroup = new FormGroup({
+		glycose: new FormControl(null, Validators.required),
+		carbs: new FormControl(null, Validators.required)
+	});
+
+	getCurrentRecord(): void {
+		this.route.params.subscribe(params => {
+			this.record_id = params.id
+		})
+		this.patientService.getPatientRecordById(this.patient_id, this.record_id).subscribe(response => {
+			this.patientRecord = response.data
+			
+			// initialize form
+			this.editPatientRecord = new FormGroup ({
+				glycose: new FormControl(response.data.glycose, Validators.required),
+				carbs: new FormControl(response.data.carbs, Validators.required)
+			})
+		})
+
+	}
+
+	editRecord(): void {
+		this.patientService.editPatientRecord(this.patient_id, this.record_id, this.editPatientRecord.get('glycose').value, this.editPatientRecord.get('carbs').value).subscribe(response => {
+			console.log(response)
+			if (response.status == 200) {
+				this.toastr.success('Record successfully edited.', 'Success', {
+					timeOut: 2000,
+					positionClass: 'toast-top-center'
+				}).onHidden.toPromise().then(_ => {
+					this.router.navigate(['/medidatarepo/profile']);
+				}).catch(error => {
+					console.log(error);
+				});
+			}
+		})
+	}
+
+	formatPatientRecordDate(date: number) {
+		return moment(date).format('DD/MM/YYYY, h:mm:ss a')
 	}
 
 }
