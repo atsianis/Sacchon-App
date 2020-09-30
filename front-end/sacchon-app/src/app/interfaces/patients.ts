4dad44a59e5c5e412891fce6b9fd340ce687b08a
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-30 14:24:57 +0300

    Rework interface to have doctor_id

--- front-end/sacchon-app/src/app/interfaces/patients.ts
+++ front-end/sacchon-app/src/app/interfaces/patients.ts
@@ -1,10 +1,11 @@
 export interface Patients {
-	id: string;
+	id: number;
 	firstName: string;
 	lastName: string;
 	email: string;
 	password: string;
 	dob: string;
+	doctor_id: number;
 	canBeExamined: string;
 	lastActive: string;
 	gender: string;
