b77304ec3de24685340d1745ba6dc4daa38cca3f
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-09-30 20:28:22 +0300

    Import new component and replace common with browser module

--- front-end/sacchon-app/src/app/medi-data-repo/medi-data-repo.module.ts
+++ front-end/sacchon-app/src/app/medi-data-repo/medi-data-repo.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PatientProfileComponent } from './profile/profile.component';
 import { PatientEditProfileComponent } from './edit-profile/edit-profile.component';
-import { PatientRecordComponent } from './patient-record/patient-record.component';
 import { AddPatientRecordComponent } from './add-patient-record/add-patient-record.component';
 import { EditPatientRecordComponent } from './edit-patient-record/edit-patient-record.component';
 import { DataTablesModule } from 'angular-datatables';
 import { ChartsModule } from 'ng2-charts';
 import { HttpClientModule } from '@angular/common/http';
 import { MediDataRepoGuard } from './medi-data-repo.guard';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ConsultationsComponent } from './consultations/consultations.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -17,22 +18,23 @@ import { MediDataRepoGuard } from './medi-data-repo.guard';
 	declarations: [
 		PatientProfileComponent,
 		PatientEditProfileComponent,
-		PatientRecordComponent,
 		AddPatientRecordComponent,
 		EditPatientRecordComponent,
+		ConsultationsComponent,
 	],
 	imports: [
-		CommonModule,
+		BrowserModule,
 		DataTablesModule,
 		ChartsModule,
+		ReactiveFormsModule,
 		HttpClientModule,
 		RouterModule.forChild([
 			{ path: 'medidatarepo/profile', component: PatientProfileComponent, canActivate: [ MediDataRepoGuard ] },
 			{ path: 'medidatarepo/profile/edit', component: PatientEditProfileComponent, canActivate: [ MediDataRepoGuard ] },
-			{ path: 'medidatarepo/record', component: PatientRecordComponent, canActivate: [ MediDataRepoGuard ] },
 			{ path: 'medidatarepo/record/add', component: AddPatientRecordComponent, canActivate: [ MediDataRepoGuard ] },
-			{ path: 'medidatarepo/record/edit', component: EditPatientRecordComponent, canActivate: [ MediDataRepoGuard ] },
-		])
+			{ path: 'medidatarepo/record/:id/edit', component: EditPatientRecordComponent, canActivate: [ MediDataRepoGuard ] },
+			{ path: 'medidatarepo/consultations', component: ConsultationsComponent, canActivate: [ MediDataRepoGuard ]}
+		]),
 	],
 	exports: [
 		RouterModule
