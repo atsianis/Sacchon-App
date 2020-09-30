c98edfd880454acc2e90feac087178aa3e3bf48b
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-30 18:36:49 +0300

    change storeData method to set the timeCreation of input in server, not from user

--- back-end/src/main/java/com/pfizer/sacchon/team3/resource/patient/PatientRecordsListImpl.java
+++ back-end/src/main/java/com/pfizer/sacchon/team3/resource/patient/PatientRecordsListImpl.java
@@ -10,7 +10,7 @@ import com.pfizer.sacchon.team3.representation.PatientRecordRepresentation;
 import com.pfizer.sacchon.team3.representation.PatientRepresentation;
 import com.pfizer.sacchon.team3.representation.ResponseRepresentation;
 import com.pfizer.sacchon.team3.resource.util.ResourceValidator;
-import org.restlet.data.Status;
+import org.jetbrains.annotations.NotNull;
 import org.restlet.engine.Engine;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
@@ -33,7 +33,7 @@ public class PatientRecordsListImpl extends ServerResource implements PatientRec
         try {
             patientRecordRepository = new PatientRecordRepository(JpaUtil.getEntityManager());
             patientRepository = new PatientRepository(JpaUtil.getEntityManager());
-            id = Long.parseLong(getAttribute("id"));
+            id = Long.parseLong(getAttribute("patient_id"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -65,14 +65,11 @@ public class PatientRecordsListImpl extends ServerResource implements PatientRec
         Optional<Patients> opatient = patientRepository.findById(id);
         if (opatient.isPresent()) {
             // Check entity
-            // Convert to PatientRepr so a validation can procceed
+            // Convert to PatientRepresentation and validate
             Patients patient = opatient.get();
-            PatientRepresentation pr = new PatientRepresentation();
-            pr.setLastName(patient.getLastName());
-            pr.setFirstName(patient.getFirstName());
             try {
                 ResourceValidator.notNull(patientRecordRepresentation);
-                ResourceValidator.validate(pr);
+                ResourceValidator.validate(setPatientRepresentation(patient));
             } catch (BadEntityException ex) {
                 return new ResponseRepresentation<>(422, "Bad Entity", null);
             }
@@ -80,50 +77,89 @@ public class PatientRecordsListImpl extends ServerResource implements PatientRec
             LOGGER.finer("Patient checked");
 
             try {
-                // Convert PatientRecordRepr to PatientRecord
-                PatientRecords patientRecordsIn = new PatientRecords();
-                patientRecordsIn.setGlycose(patientRecordRepresentation.getGlycose());
-                patientRecordsIn.setCarbs(patientRecordRepresentation.getCarbs());
-                patientRecordsIn.setTimeCreated(patientRecordRepresentation.getTimeCreated());
-                patientRecordsIn.setPatient(patient);
+                PatientRecords patientRecordsIn = getPatientRecordsIn(patientRecordRepresentation, patient);
 
-                // Check if patient can add PatientRecord
-                boolean lastConsultationLessThanAMonthAgo = patientRepository.checkLastConsultation(patientRecordsIn, patient.getConsultations());
+                boolean lastConsultationLessThanAMonthAgo=true;
+                if (patient.getConsultations().size()!=0)
+                    lastConsultationLessThanAMonthAgo = patientRepository.checkLastConsultation(patient.getConsultations());
                 boolean recordTimeMoreRecentThanPatientsCreationTime = patientRepository.checkPatientsCreationTime(patientRecordsIn, patient.getTimeCreated());
 
                 if (lastConsultationLessThanAMonthAgo && recordTimeMoreRecentThanPatientsCreationTime) {
-                    Optional<PatientRecords> patientRecordsOut = patientRecordRepository.save(patientRecordsIn);
-                    PatientRecords patientRecords = null;
-                    if (patientRecordsOut.isPresent()) {
-                        patientRecords = patientRecordsOut.get();
+
+                    Optional<PatientRecords> patientRecordOut = patientRecordRepository.save(patientRecordsIn);
+                    PatientRecords patientRecord;
+                    if (patientRecordOut.isPresent()) {
+                        patientRecord = patientRecordOut.get();
                         // change patients last activity field
                         patient.setLastActive(new Date());
                         patientRepository.update(patient);
                     } else
                         return new ResponseRepresentation<>(404, "Record not found", null);
 
-                    //Convert PatientRecord to PatientRecordRepr
-                    PatientRecordRepresentation result = new PatientRecordRepresentation();
-                    result.setGlycose(patientRecords.getGlycose());
-                    result.setCarbs(patientRecords.getCarbs());
-                    result.setTimeCreated(patientRecords.getTimeCreated());
-                    result.setId(patientRecords.getId());
-
-                    getResponse().setLocationRef("http://localhost:9000/v1/patient/" + patient.getId() + "/storeData/patientRecord/" + patientRecords.getId());
-                    getResponse().setStatus(Status.SUCCESS_CREATED);
-
+                    PatientRecordRepresentation result = getPatientRecordRepresentationOut(patientRecord);
                     LOGGER.finer("Record successfully added.");
 
-                    return new ResponseRepresentation<>(404, "Record created", result);
+                    return new ResponseRepresentation<>(200, "Record created", result);
                 } else {
                     return new ResponseRepresentation<>(422, "Bad Record Date", null);
                 }
-
             } catch (Exception ex) {
-                throw new ResourceException(ex);
+                return new ResponseRepresentation<>(422, "Bad Record Date", null);
             }
         } else {
             return new ResponseRepresentation<>(404, "Not found", null);
         }
+    }
+
+    /**
+     *
+     * @param patient
+     * @return PatientRepresentation
+     *
+     * Convert the Patient into a PatientRepresentation in order to validate his fields
+     */
+    @NotNull
+    private PatientRepresentation setPatientRepresentation(Patients patient) {
+        PatientRepresentation pr = new PatientRepresentation();
+        pr.setLastName(patient.getLastName());
+        pr.setFirstName(patient.getFirstName());
+        return pr;
+    }
+
+    /**
+     *
+     * @param patientRecord
+     * @return PatientRecordRepresentation object
+     *
+     * Convert the persisted PatientRecord to the PatientRecordRepresentation
+     * that will be returned to the user
+     */
+    @NotNull
+    private PatientRecordRepresentation getPatientRecordRepresentationOut(PatientRecords patientRecord) {
+        PatientRecordRepresentation result = new PatientRecordRepresentation();
+        result.setGlycose(patientRecord.getGlycose());
+        result.setCarbs(patientRecord.getCarbs());
+        result.setTimeCreated(patientRecord.getTimeCreated());
+        result.setId(patientRecord.getId());
+        return result;
+    }
+
+    /**
+     *
+     * @param patientRecordRepresentation
+     * @param patient
+     * @return PatientRecord
+     *
+     * Converts the input of the user into a PatientRecord object
+     * that is ready to be persisted
+     */
+    @NotNull
+    private PatientRecords getPatientRecordsIn(PatientRecordRepresentation patientRecordRepresentation, Patients patient) {
+        PatientRecords patientRecordsIn = new PatientRecords();
+        patientRecordsIn.setGlycose(patientRecordRepresentation.getGlycose());
+        patientRecordsIn.setCarbs(patientRecordRepresentation.getCarbs());
+        patientRecordsIn.setTimeCreated(new Date());
+        patientRecordsIn.setPatient(patient);
+        return patientRecordsIn;
     }
 }
