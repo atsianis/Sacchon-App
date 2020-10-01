7a5a6131ae50fb5a68c3546741869faf16be7334
Michael Koptsis <mkoptsis@athtech.gr>
2020-10-01 17:40:40 +0300

    implement initialize form on reporter

--- front-end/sacchon-app/src/app/reporter/edit-profile/edit-profile.component.ts
+++ front-end/sacchon-app/src/app/reporter/edit-profile/edit-profile.component.ts
@@ -19,15 +19,16 @@ export class EditProfileComponent implements OnInit {
   password = sessionStorage.getItem('password');
 
 	reporterEdit = new FormGroup({
-		firstame: new FormControl(this.firstName, [Validators.required]),
-		lastName: new FormControl(this.lastName, [Validators.required]),
-		email: new FormControl(this.email, [Validators.required, Validators.email]),
+		firstame: new FormControl(null, [Validators.required]),
+		lastName: new FormControl(null, [Validators.required]),
+		email: new FormControl(null, [Validators.required, Validators.email]),
 		password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
 		passwordconfirm: new FormControl(null)
 	});
 
 
 	ngOnInit(): void {
+		this.initializeForm()
 	 }
 
 	edit(): void {
@@ -52,5 +53,15 @@ export class EditProfileComponent implements OnInit {
 
 	cancel(): void {
 		this.router.navigate(['/reporter'])
+	}
+
+	initializeForm(): void {
+		this.reporterEdit = new FormGroup({
+			firstName: new FormControl(this.firstName, [Validators.required]),
+			lastName: new FormControl(this.lastName, [Validators.required]),
+			email: new FormControl(this.email, [Validators.required, Validators.email]),
+			password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+			passwordconfirm: new FormControl(null)
+		});
 	}
 }
