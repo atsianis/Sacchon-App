127cf116a13afa340f9c60e6ac44d72b2b8be6d7
Michael Koptsis <mkoptsis@athtech.gr>
2020-10-01 14:54:17 +0300

    edit reporter profil and buttons

--- front-end/sacchon-app/src/app/reporter/edit-profile/edit-profile.component.ts
+++ front-end/sacchon-app/src/app/reporter/edit-profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -10,7 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor(private toastr: ToastrService, private http: HttpClient) { }
+  constructor(private router: Router, private toastr: ToastrService, private http: HttpClient) { }
 
   firstName = sessionStorage.getItem('firstName');
   lastName = sessionStorage.getItem('lastName');
@@ -47,5 +48,9 @@ export class EditProfileComponent implements OnInit {
 		} else {
 			this.reporterEdit.markAllAsTouched();
 		}
+	}
+
+	cancel(): void {
+		this.router.navigate(['/reporter'])
 	}
 }
