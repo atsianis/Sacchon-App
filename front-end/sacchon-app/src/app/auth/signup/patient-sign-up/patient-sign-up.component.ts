878370f49111015d5baa63b7b1f99c782e166dbf
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-10-01 23:34:39 +0300

    Implement patient sign up

--- front-end/sacchon-app/src/app/auth/signup/patient-sign-up/patient-sign-up.component.ts
+++ front-end/sacchon-app/src/app/auth/signup/patient-sign-up/patient-sign-up.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../auth.service';
 
 @Component({
 	selector: 'sacchon-app-patient-sign-up',
@@ -10,7 +11,7 @@ import { ToastrService } from 'ngx-toastr';
 	styleUrls: ['./patient-sign-up.component.scss'],
 })
 export class PatientSignUpComponent implements OnInit {
-	constructor(private toastr: ToastrService, private http: HttpClient, private router: Router) { }
+	constructor(private toastr: ToastrService, private authService: AuthService, private router: Router) { }
 
 	patientSignUp = new FormGroup({
 		firstName: new FormControl(null, [Validators.required]),
@@ -27,23 +28,23 @@ export class PatientSignUpComponent implements OnInit {
 	ngOnInit(): void { }
 
 	signUp(): void {
-		if (this.patientSignUp.get('password').value === this.patientSignUp.get('passwordconfirm').value) {
-			this.http.post(`http://localhost:9000/v1/register/patient`, {
-				firstName: this.patientSignUp.get('firstName').value,
-				lastName: this.patientSignUp.get('lastName').value,
-				email: this.patientSignUp.get('email').value,
-				password: this.patientSignUp.get('password').value,
-				dob: "1994-01-10", //WIP: add datepicker
-				gender: this.patientSignUp.get('gender').value
-			}).subscribe(response => {
+		if (this.patientSignUp.valid && (this.patientSignUp.get('password').value === this.patientSignUp.get('passwordconfirm').value)) {
+			const firstName = this.patientSignUp.get('firstName').value;
+			const lastName = this.patientSignUp.get('lastName').value;
+			const email = this.patientSignUp.get('email').value;
+			const password = this.patientSignUp.get('password').value;
+			const unformattedDob = this.patientSignUp.get('dob').value;
+			const gender = this.patientSignUp.get('gender').value;
+			
+			const dob = new Date(unformattedDob.year, unformattedDob.month, unformattedDob.day);
+
+			this.authService.signUpPatient(firstName, lastName, dob, gender, email, password).subscribe(response => {
+				console.log(response)
 				this.toastr.success('You will be redirected to login page soon.', 'Successfully registered', {
 					timeOut: 2000,
 					positionClass: 'toast-top-center'
 				}).onHidden.toPromise().then(_ => {
 					this.router.navigate(['/login/medidatarepo'])
-				}, error => {
-					console.log(error)
-					// this.toastr.error(response)
 				});
 			}, (error) => {
 				console.log(error)
