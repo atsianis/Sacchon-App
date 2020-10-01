6f498c373a60682f592fba3a06e77ddfec9a88a2
Kalaitzidis Alexandros <alexkalaitzidis2@gmail.com>
2020-10-01 14:15:59 +0300

    Remove console.log

--- front-end/sacchon-app/src/app/reporter/all-doctors-db/all-doctors-db.component.ts
+++ front-end/sacchon-app/src/app/reporter/all-doctors-db/all-doctors-db.component.ts
@@ -28,7 +28,6 @@ export class AllDoctorsDbComponent implements OnInit {
 
 	getDoctorsDB(): void {
 		this.reporterService.getAllDoctorsFromDatabase().subscribe(doctors => {
-			console.log(doctors.data)
 			this.doctors = doctors.data;
 			this.dtTrigger.next();
 		}, (err) => {
